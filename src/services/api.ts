516c9cdf71a4216081f7493df1512d24c33cb046
ytraddan <1nightshelter@gmail.com>
2024-12-07 13:34:34 +0300

    add error handling

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,22 +1,25 @@
 import { Product } from "@/types/product";
-
+  
 const API_URL = "https://fakestoreapi.com";
 
 export async function getProducts(): Promise<Product[]> {
   const res = await fetch(`${API_URL}/products`);
+  if (!res.ok) {
+    throw new Error("Failed to fetch products");
+  }
   return res.json();
 }
 
-export async function getProduct(id: number): Promise<Product> {
-  const res = await fetch(`${API_URL}/products/${id}`);
-  return res.json();
-}
-
-export async function createProduct(product: Omit<Product, "id">): Promise<number> {
+export async function createProduct(
+  product: Omit<Product, "id">,
+): Promise<number> {
   const res = await fetch(`${API_URL}/products`, {
     method: "POST",
     body: JSON.stringify(product),
   });
+  if (!res.ok) {
+    throw new Error("Failed to create product");
+  }
   return res.json();
 }
 
@@ -24,5 +27,8 @@ export async function deleteProduct(id: number): Promise<Product> {
   const res = await fetch(`${API_URL}/products/${id}`, {
     method: "DELETE",
   });
+  if (!res.ok) {
+    throw new Error("Failed to delete product");
+  }
   return res.json();
 }
