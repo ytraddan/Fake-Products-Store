7278c5f1fbf9d21f3899549ce4f2fcdd0e7be9ec
ytraddan <1nightshelter@gmail.com>
2024-12-07 13:52:23 +0300

    add root route

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { store } from "./state/store.ts";
-import { BrowserRouter, Route, Routes } from "react-router";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router";
 import { Products } from "./pages/Products.tsx";
 import { ProductDetails } from "./pages/ProductDetails.tsx";
 import { CreateProduct } from "./pages/CreateProduct.tsx";
@@ -17,6 +17,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     <Provider store={store}>
       <BrowserRouter>
         <Routes>
+          <Route path="/" element={<Navigate to="/products" replace />} />
           <Route path="/products" element={<Products />} />
           <Route path="/products/:id" element={<ProductDetails />} />
           <Route path="/create-product" element={<CreateProduct />} />
