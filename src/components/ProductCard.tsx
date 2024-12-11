9121205c758e2c068dd7adfc31f01338e7d7e303
ytraddan <1nightshelter@gmail.com>
2024-12-11 19:59:16 +0300

    move product card component to it's own file

---
+++ src/components/ProductCard.tsx
@@ -0,0 +1,84 @@
+import { useNavigate } from "react-router";
+import { Product } from "@/types/product";
+import { useDispatch } from "react-redux";
+import { toggleFavorite } from "@/state/productsSlice";
+import { RootState } from "@/state/store";
+import { deleteProduct } from "@/state/productsSlice";
+import { AppDispatch } from "@/state/store";
+import { useSelector } from "react-redux";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import { LoadingImage } from "@/components/ui/LoadingImage";
+import { LikeButton } from "@/components/ui/LikeButton";
+
+export const ProductCard = ({ product }: { product: Product }) => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch<AppDispatch>();
+  const { favorites } = useSelector((state: RootState) => state.products);
+
+  const handleCardClick = (id: number) => {
+    navigate(`/products/${id}`);
+  };
+
+  const handleDelete = (id: number) => {
+    dispatch(deleteProduct(id));
+  };
+
+  const handleToggleFavorite = (id: number) => {
+    dispatch(toggleFavorite(id));
+  };
+
+  const isLiked = (id: number) => {
+    return favorites.includes(id);
+  };
+
+  return (
+    <Card>
+      <CardHeader className="p-4">
+        <CardTitle className="flex items-center justify-between">
+          <span className="max-w-64 truncate">{product.title}</span>
+          <span>${product.price}</span>
+        </CardTitle>
+      </CardHeader>
+      <CardContent className="px-4 pb-4 pt-2">
+        <div
+          className="cursor-pointer"
+          onClick={() => handleCardClick(product.id)}
+        >
+          <LoadingImage
+            src={product.image}
+            alt={product.title}
+            className="mb-4 h-20 sm:h-44"
+          />
+          <p className="line-clamp-2 text-sm text-gray-500 first-letter:uppercase">
+            {product.description}
+          </p>
+        </div>
+        <div className="mt-2 flex items-center justify-between">
+          <div className="flex gap-1 sm:gap-2">
+            <Button
+              variant="outline"
+              size="sm"
+              className="p-2 sm:p-3"
+              onClick={() => navigate(`/products/${product.id}/edit`)}
+            >
+              Edit
+            </Button>
+            <Button
+              variant="ghost"
+              size="sm"
+              className="p-2 sm:p-3"
+              onClick={() => handleDelete(product.id)}
+            >
+              Delete
+            </Button>
+          </div>
+          <LikeButton
+            isLiked={isLiked(product.id)}
+            onClick={() => handleToggleFavorite(product.id)}
+          />
+        </div>
+      </CardContent>
+    </Card>
+  );
+};
--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -1,17 +1,14 @@
 import { useState } from "react";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router";
-import { deleteProduct, toggleFavorite } from "@/state/productsSlice";
-import { useDispatch } from "react-redux";
-import { AppDispatch } from "@/state/store";
-import { Product } from "@/types/product";
 import { ThemeToggle } from "@/components/ui/ThemeToggle";
 import { Input } from "@/components/ui/input";
 import { useMediaQuery } from "@/hooks/useMediaQuery";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { Skeleton } from "@/components/ui/skeleton";
 import { LikeButton } from "@/components/ui/LikeButton";
+import { ProductCard } from "@/components/ProductCard";
 import { RootState } from "@/state/store";
 import {
   Pagination,
@@ -28,8 +25,6 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
-import { LoadingImage } from "@/components/ui/LoadingImage";
-
 
 type PageHeaderProps = {
   showFavorites: boolean;
@@ -189,7 +184,7 @@ const PageHeader = ({
   handleToggleShowFavorites,
 }: PageHeaderProps) => {
   const navigate = useNavigate();
-  
+
   return (
     <div className="flex items-start justify-between">
       <h1 className="text-2xl font-bold dark:text-white">
@@ -247,78 +242,6 @@ const CategoryFilter = ({
         ))}
       </SelectContent>
     </Select>
-  );
-};
-
-const ProductCard = ({ product }: { product: Product }) => {
-  const navigate = useNavigate();
-  const dispatch = useDispatch<AppDispatch>();
-  const { favorites } = useSelector((state: RootState) => state.products);
-
-  const handleCardClick = (id: number) => {
-    navigate(`/products/${id}`);
-  };
-
-  const handleDelete = (id: number) => {
-    dispatch(deleteProduct(id));
-  };
-
-  const handleToggleFavorite = (id: number) => {
-    dispatch(toggleFavorite(id));
-  };
-
-  const isLiked = (id: number) => {
-    return favorites.includes(id);
-  };
-
-  return (
-    <Card className="overflow-hidden">
-      <CardHeader className="p-4">
-        <CardTitle className="flex items-center justify-between">
-          <span className="max-w-64 truncate">{product.title}</span>
-          <span>${product.price}</span>
-        </CardTitle>
-      </CardHeader>
-      <CardContent className="px-4 pb-4 pt-2">
-        <div
-          className="cursor-pointer"
-          onClick={() => handleCardClick(product.id)}
-        >
-          <LoadingImage
-            src={product.image}
-            alt={product.title}
-            className="mb-4 h-20 sm:h-44"
-          />
-          <p className="line-clamp-2 text-sm text-gray-500 first-letter:uppercase">
-            {product.description}
-          </p>
-        </div>
-        <div className="mt-2 flex items-center justify-between">
-          <div className="flex gap-1 sm:gap-2">
-            <Button
-              variant="outline"
-              size="sm"
-              className="p-2 sm:p-3"
-              onClick={() => navigate(`/products/${product.id}/edit`)}
-            >
-              Edit
-            </Button>
-            <Button
-              variant="ghost"
-              size="sm"
-              className="p-2 sm:p-3"
-              onClick={() => handleDelete(product.id)}
-            >
-              Delete
-            </Button>
-          </div>
-          <LikeButton
-            isLiked={isLiked(product.id)}
-            onClick={() => handleToggleFavorite(product.id)}
-          />
-        </div>
-      </CardContent>
-    </Card>
   );
 };
 
